99b1aa98330c6dcaa1a9291ad3d20d06b5a7dd74
Aislu Baisova <ice_lu_93@mail.ru>
2022-08-12 18:33:38 +0300

    bring it on task

--- test/specs/iCanWinTask.test.js
+++ test/specs/iCanWinTask.test.js
@@ -1,14 +1,14 @@
-describe("I can win task", () => {
+// describe("I can win task", () => {
     
-    it("Creat new paste", async () => {
-    await browser.url("https://pastebin.com");
-    await $(`//textarea[@id="postform-text"]`).setValue("Hello from WebDriver");
-    await $(`#select2-postform-expiration-container`).click();
-    let neededExpiration = await $(`#select2-postform-expiration-results :nth-child(3)`);
-    await neededExpiration.waitForClickable();
-    await neededExpiration.click();
-    await $(`[class="form-control"]`).setValue("helloweb");
-    await $(`//button[@type="submit"]`).click();
-    await expect(browser).toHaveTitle("helloweb - Pastebin.com");
-    })
-})
+//     it("Creat new paste", async () => {
+//     await browser.url("https://pastebin.com");
+//     await $(`//textarea[@id="postform-text"]`).setValue("Hello from WebDriver");
+//     await $(`#select2-postform-expiration-container`).click();
+//     let neededExpiration = await $(`#select2-postform-expiration-results :nth-child(3)`);
+//     await neededExpiration.waitForClickable();
+//     await neededExpiration.click();
+//     await $(`[class="form-control"]`).setValue("helloweb");
+//     await $(`//button[@type="submit"]`).click();
+//     await expect(browser).toHaveTitle("helloweb - Pastebin.com");
+//     })
+// })
