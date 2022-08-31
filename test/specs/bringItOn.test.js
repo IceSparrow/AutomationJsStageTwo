667113ad7acef7d60da094a2a0def93e5d72ec56
Aislu Baisova <ice_lu_93@mail.ru>
2022-09-01 00:03:38 +0300

    hardcore done

--- test/specs/bringItOn.test.js
+++ test/specs/bringItOn.test.js
@@ -1,30 +1,30 @@
-// import PastebinPage from "./../../pageobjects/pastebinPage";
+import PastebinPage from "./../../pageobjects/pastebinPage";
 
 
-// describe("Bring it on", () => {
+describe("Bring it on", () => {
     
-//     it("Creat new paste", async () => {
-//     await PastebinPage.open();
-//     await PastebinPage.pasteArea.setValue(PastebinPage.pasteText);
+    it("Creat new paste", async () => {
+    await PastebinPage.open();
+    await PastebinPage.pasteArea.setValue(PastebinPage.pasteText);
 
-//     await PastebinPage.syntaxHighlighting.waitForClickable();
-//     await PastebinPage.syntaxHighlighting.click();
-//     await PastebinPage.highlighterInput.addValue("Bash");
-//     await PastebinPage.highlighterInput.keys('\uE007');
+    await PastebinPage.syntaxHighlighting.waitForClickable();
+    await PastebinPage.syntaxHighlighting.click();
+    await PastebinPage.highlighterInput.addValue("Bash");
+    await PastebinPage.highlighterInput.keys('\uE007');
 
-//     await PastebinPage.expirationList.waitForClickable();
-//     await PastebinPage.expirationList.click();
+    await PastebinPage.expirationList.waitForClickable();
+    await PastebinPage.expirationList.click();
 
-//     await PastebinPage.neededExpiration.waitForClickable();
-//     await PastebinPage.neededExpiration.click();
+    await PastebinPage.neededExpiration.waitForClickable();
+    await PastebinPage.neededExpiration.click();
 
-//     await PastebinPage.pasteName.setValue(PastebinPage.title);
-//     await PastebinPage.createPasteBtn.waitForClickable();
-//     await PastebinPage.createPasteBtn.click();
+    await PastebinPage.pasteName.setValue(PastebinPage.title);
+    await PastebinPage.createPasteBtn.waitForClickable();
+    await PastebinPage.createPasteBtn.click();
 
-//     await browser.refresh();
-//     await expect(browser).toHaveTitleContaining(PastebinPage.title);
-//     await expect (PastebinPage.bashHighlighter).toExist();
-//     await expect (PastebinPage.pasteTextElement).toHaveValueContaining(PastebinPage.pasteText);
-//     })
-// })
+    await browser.refresh();
+    await expect(browser).toHaveTitleContaining(PastebinPage.title);
+    await expect (PastebinPage.bashHighlighter).toExist();
+    await expect (PastebinPage.pasteTextElement).toHaveValueContaining(PastebinPage.pasteText);
+    })
+})
